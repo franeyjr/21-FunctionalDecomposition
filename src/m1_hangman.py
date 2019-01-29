1ced3483e67cfe4a6149a913b42c9407ba6df617
franeyjr <franeyjr@rose-hulman.edu>
2019-01-29 17:38:44 -0500

    NOT DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -11,23 +11,30 @@ Authors: Jack Franey and Jake Lauteri and Josiah Hasegawa.
 import random
 
 def main():
-    print('boobs')
-    # get_min_length()
-    #print(get_word())
-    secret_word = get_word()
-    list = [spaces(secret_word)]
-    print(list)
-    guess(secret_word)
+    min_length = get_min_length()
+    secret_word = get_word(min_length)
+    game_loop(secret_word)
 
 
+    # list = [spaces(secret_word)]
+    # print(list)
+    #
+    # lst1 = []
+    # for j in range(5+ len(secret_word)):
+    #     lst1 = lst1 + [guess(secret_word)]
+    #
+    # for q in range(len(lst1)):
+    #     for t in range(len(lst1[q])):
+    #         change_word(lst1, list, secret_word[lst1[q][t]])
 
-def get_word():
+
+def get_word(min_length):
 
     with open('words.txt') as f:
         f.readline()
         string = f.read()
         words = string.split()
-    min_length = get_min_length()
+    # min_length = get_min_length()
     while True:
 
         r = random.randrange(0,len(words))
@@ -36,33 +43,49 @@ def get_word():
         if len(secret_word) >= min_length:
             return secret_word
 
-
-
-
 def get_min_length():
 
     min_length = int(input('Enter the minimum length of the secret word:'))
     print(min_length)
     return min_length
 
-def guess(secret_word):
-    lst = []
-    g = str(input('Enter a letter: '))
+def game_loop(secret_word):
+    chances = 5
+    spaces = []
+    for _ in range(len(secret_word)):
+        spaces = spaces + ['_']
+
+    letter = guess()
+    letter, k = check_guess(secret_word,letter,chances)
+    spaces, letter = change_word(secret_word,letter,spaces)
+
+def guess():
+
+    letter = str(input('Enter a letter: '))
+    return letter
+
+def check_guess(secret_word,letter,chances):
     for k in range(len(secret_word)):
-        if secret_word[k] == g:
-            lst = lst + [k]
-    return lst
+        if secret_word[k] == letter:
+            return letter, k
+        else:
+            chances = chances - 1
+            return chances, None
 
-def spaces(secret_word):
-
-    list = []
+def change_word(secret_word,letter,spaces):
+    right = []
     for k in range(len(secret_word)):
-        list = list + ['*']
+        if secret_word[k] == letter:
+            right = right + [secret_word[k]]
+    for j in range(len(right)):
+        if letter == right[j]:
+            spaces[j] = letter
+    return spaces, letter
 
-    return list
-
-def change_word(some_list, secret_word, w):
-
+    # for k in range(len(some_list)):
+    #     secret_word[some_list[k]] = (w)
+    # print(secret_word)
+    # return secret_word
 
 
 
